8f87ccf55795f4e4ae57f3b2f006502246275ff2
Kevin Krummenauer <lopusknut@googlemail.com>
2016-05-01 01:50:04 +0200

    Update entrypoint.sh

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -3,6 +3,10 @@
 if [ "$SSHUSER" != "root" ]; then
   groupadd $SSHUSER
   useradd -d /home/$SSHUSER -g $SSHUSER -m $SSHUSER
+  
+  if [ "$SHELLFORWARD" = "yes" ]; then
+    sed -i "s|/home/$SSHUSER:|/home/$SSHUSER:/data/resources/shellforward/client|g" /etc/passwd
+  fi
 fi
 
 echo "$SSHUSER:$SSHPASSWD" | chpasswd
